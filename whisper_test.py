e372c7464f4e42d2dde536b1afbe167d998a50d5
YL-Tan <ytan@seattleu.edu>
2023-01-29 19:59:38 -0800

    Cleaned unwanted files and formatted the output

--- whisper_test.py
+++
@@ -1,9 +0,0 @@
-"""
-A basic demonstration to show how OpenAI's Whisper can be used to 
-accurately transcribe and perform ASR.
-"""
-import whisper
-
-model = whisper.load_model("base")
-result = model.transcribe("anjali_zoom_test.m4a")
-print(result["text"])
