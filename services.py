d900fac222ada13910f921f46cc84386bf93dd8b
YL-Tan <ytan@seattleu.edu>
2023-02-08 01:39:02 -0800

    Hooked up summary and transcription with Django on 2nd tab

--- services.py
+++ services.py
@@ -1,10 +1,82 @@
-
+import torch
+from transformers import BartForConditionalGeneration, BartTokenizer
 import whisper, pytube, hashlib, os, datetime
 from pytube import YouTube
 from nltk.tokenize import sent_tokenize
 from  mangorest.mango import webapi
+import pyannote
+from pyannote.audio import Pipeline
+from pyannote.core import Segment, Annotation, Timeline
+import json
+from django.http import HttpResponse
 
-model = whisper.load_model("base")
+# Taken from https://github.com/yinruiqing/pyannote-whisper
+class PyanWhisper:
+    PUNC_SENT_END = ['.', '?', '!']
+        
+    def diarize_text(transcribe_res, diarization_result):
+        timestamp_texts = PyanWhisper.get_text_with_timestamp(transcribe_res)
+        spk_text = PyanWhisper.add_speaker_info_to_text(timestamp_texts, diarization_result)
+        res_processed = PyanWhisper.merge_sentence(spk_text)
+        return res_processed
+
+    def get_text_with_timestamp(transcribe_res):
+        timestamp_texts = []
+        for item in transcribe_res['segments']:
+            start = item['start']
+            end = item['end']
+            text = item['text']
+            timestamp_texts.append((Segment(start, end), text))
+        return timestamp_texts
+    
+    def add_speaker_info_to_text(timestamp_texts, ann):
+        spk_text = []
+        for seg, text in timestamp_texts:
+            spk = ann.crop(seg).argmax()
+            spk_text.append((seg, spk, text))
+        return spk_text
+    
+    def merge_cache(text_cache):
+        sentence = ''.join([item[-1] for item in text_cache])
+        spk = text_cache[0][1]
+        start = text_cache[0][0].start
+        end = text_cache[-1][0].end
+        return Segment(start, end), spk, sentence
+    
+    def merge_sentence(spk_text):
+        merged_spk_text = []
+        pre_spk = None
+        text_cache = []
+        for seg, spk, text in spk_text:
+            if spk != pre_spk and pre_spk is not None and len(text_cache) > 0:
+                merged_spk_text.append(PyanWhisper.merge_cache(text_cache))
+                text_cache = [(seg, spk, text)]
+                pre_spk = spk
+            elif text[-1] in PyanWhisper.PUNC_SENT_END:
+                text_cache.append((seg, spk, text))
+                merged_spk_text.append(PyanWhisper.merge_cache(text_cache))
+                text_cache = []
+                pre_spk = spk
+            else:
+                text_cache.append((seg, spk, text))
+                pre_spk = spk
+        if len(text_cache) > 0:
+            merged_spk_text.append(PyanWhisper.merge_cache(text_cache))
+        return merged_spk_text
+
+    def write_to_txt(spk_sent, file):
+        with open(file, 'w') as fp:
+            for seg, spk, sentence in spk_sent:
+                line = f'{seg.start:.2f} {seg.end:.2f} {spk} {sentence}\n'
+                fp.write(line)
+
+# our models                
+model = whisper.load_model("base", device="cuda")
+diarizer = Pipeline.from_pretrained("pyannote/speaker-diarization@2.1",
+                                    use_auth_token="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")
+
+bart = BartForConditionalGeneration.from_pretrained("facebook/bart-large-cnn")
+tokenizer = BartTokenizer.from_pretrained("facebook/bart-large-cnn")
 
 def transcribe_file(file ="/Users/snarayan/Desktop/data/audio/index.mp4", **kwargs):
     result = model.transcribe(file)
@@ -56,14 +128,73 @@ def transcribe_youtube( url = test_url , force_download=False, force_transribe=F
 
 
 #--------------------------------------------------------------------------------------------------------    
-@webapi("/parrot/myname/")
-def myName( n = "sada", **kwargs):
+@webapi("/parrot/transcribe_wavinput/")
+def transcribe_wavinput(url, **kwargs):
+    print("Hi: " + url)
+    if url.method == 'POST':
+        file = request.FILES['file']
+        print("I'm in")
+    # ret = "\n\n My Name is: " + n + "\n"
+    # for g in kwargs:
+    #     if (g =="request"):
+    #         continue;
+    #     ret += g + " " + kwargs.get(g) + "\n"
     
-    ret = "\n\n My Name is: " + n + "\n"
-    for g in kwargs:
-        if (g =="request"):
-            continue;
-        ret += g + " " + kwargs.get(g) + "\n"
+    # return ret
+#--------------------------------------------------------------------------------------------------------    
+@webapi("/parrot/uploadfile")
+def uploadfile(request,  **kwargs):
+    par = dict(request.GET)
+    par.update(request.POST)
+
+    DESTDIR ="/tmp/parrot/"
+    print("uploadfile : ", DESTDIR, kwargs)
     
+    if (not os.path.exists(DESTDIR)):
+        os.makedirs(DESTDIR)
+    
+    
+    ret = "Files:\n"
+    for f in request.FILES.getlist('file'):
+        content = f.read()
+        filename = f"{DESTDIR}{str(f)}"
+        print(f"++ Save file {filename} Content: {len(content)} :")
+        with open(filename, "wb") as f:
+            f.write(content)
+        ret += filename + "\n"
+
+    print(" Retuning ", ret )
     return ret
+
+#--------------------------------------------------------------------------------------------------------    
+@webapi("/parrot/processfile")
+def processfile(request, force_transribe=False, **kwargs):
+    print("processing file: ", kwargs)
+
+    ret = uploadfile(request, **kwargs)
+    f = ret.split('\n')[1]
+
+
+    print( f"Calling transcription: {f}")
+    result = model.transcribe(f)
+    diarization = diarizer(f)
+    final_result = PyanWhisper.diarize_text(result, diarization) 
+    ret = ""
+    # Write to a new file
+    with open(f +".txt", "w") as new_f:
+        for seg, spk, sent in final_result:
+            line = f'{spk}:{sent}\n'
+            new_f.write(line)                               
+            ret += line
+        transcription = ret
     
+    input_ids = tokenizer.encode(transcription, return_tensors="pt", truncation=True)
+    
+    with torch.no_grad():
+        outputs = bart.generate(input_ids)
+    # Generate Summary
+    summary = tokenizer.decode(outputs[0], skip_special_tokens=True)
+    
+    print("\n\n" + summary)
+    response = {'transcription': transcription, 'summary': summary}
+    return HttpResponse(json.dumps(response), content_type='application/json')    
