4f0de99d4a4e14fdcc0f01a3d73ca6f008c87f04
sada-narayanappa <sada.narayanappa@gmail.com>
2023-02-09 12:41:00 -0700

    Commiting

--- services.py
+++ services.py
@@ -1,14 +1,78 @@
-#!/usr/bin/env python
-#%%writefile "/tmp/trans.py"
-
-import whisper, pytube, hashlib, os, datetime, sys, re, json, argparse
+from django.http import HttpResponse
+from mangorest.mango import webapi
 from pytube import YouTube
-from nltk.tokenize import sent_tokenize
-from  mangorest.mango import webapi
-import colabexts
-import colabexts.jcommon
+from pyannote.audio import Pipeline
+from pyannote.core import Segment, Annotation, Timeline
+from transformers import BartForConditionalGeneration, BartTokenizer
+import whisper, pytube, hashlib, os, datetime, json, torch, pyannote
+from transformers import pipeline
 
-model =None
+# Taken from https://github.com/yinruiqing/pyannote-whisper
+class PyanWhisper:
+    PUNC_SENT_END = ['.', '?', '!']
+        
+    def diarize_text(transcribe_res, diarization_result):
+        timestamp_texts = PyanWhisper.get_text_with_timestamp(transcribe_res)
+        spk_text = PyanWhisper.add_speaker_info_to_text(timestamp_texts, diarization_result)
+        res_processed = PyanWhisper.merge_sentence(spk_text)
+        return res_processed
+
+    def get_text_with_timestamp(transcribe_res):
+        timestamp_texts = []
+        for item in transcribe_res['segments']:
+            start = item['start']
+            end = item['end']
+            text = item['text']
+            timestamp_texts.append((Segment(start, end), text))
+        return timestamp_texts
+    
+    def add_speaker_info_to_text(timestamp_texts, ann):
+        spk_text = []
+        for seg, text in timestamp_texts:
+            spk = ann.crop(seg).argmax()
+            spk_text.append((seg, spk, text))
+        return spk_text
+    
+    def merge_cache(text_cache):
+        sentence = ''.join([item[-1] for item in text_cache])
+        spk = text_cache[0][1]
+        start = text_cache[0][0].start
+        end = text_cache[-1][0].end
+        return Segment(start, end), spk, sentence
+    
+    def merge_sentence(spk_text):
+        merged_spk_text = []
+        pre_spk = None
+        text_cache = []
+        for seg, spk, text in spk_text:
+            if spk != pre_spk and pre_spk is not None and len(text_cache) > 0:
+                merged_spk_text.append(PyanWhisper.merge_cache(text_cache))
+                text_cache = [(seg, spk, text)]
+                pre_spk = spk
+            elif text[-1] in PyanWhisper.PUNC_SENT_END:
+                text_cache.append((seg, spk, text))
+                merged_spk_text.append(PyanWhisper.merge_cache(text_cache))
+                text_cache = []
+                pre_spk = spk
+            else:
+                text_cache.append((seg, spk, text))
+                pre_spk = spk
+        if len(text_cache) > 0:
+            merged_spk_text.append(PyanWhisper.merge_cache(text_cache))
+        return merged_spk_text
+
+    def write_to_txt(spk_sent, file):
+        with open(file, 'w') as fp:
+            for seg, spk, sentence in spk_sent:
+                line = f'{seg.start:.2f} {seg.end:.2f} {spk} {sentence}\n'
+                fp.write(line)
+            
+device = "cpu"
+if (torch.cuda.is_available() ):
+    device = "cuda"
+
+
+model = whisper.load_model("base", device=device)
 
 def getmodel():
     global model;
@@ -98,7 +162,6 @@ def uploadfile(request,  **kwargs):
     print("Retuning ", ret)
     return ret
 
-<<<<<<< HEAD
 #-----------------------------------------------------------------------------------
 def process(sysargs):
     print("Parsing and processing")
