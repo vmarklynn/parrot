c7aeb0eb4d4f345670c87a46da36c2f3e2b835ac
YL-Tan <ytan@seattleu.edu>
2023-02-12 17:12:25 -0800

    Disabled services.py

--- __init__.py
+++ __init__.py
@@ -1,8 +1,9 @@
 import os
 
 print ("Initializing Speech To Text folder: " + os.getcwd())
-if (os.path.exists("parrot/services.py")):
-    from . import services
+if (os.path.exists("parrot/services.py") and os.path.exists("parrot/transcribe_service.py")):
+    # from . import services
+    from . import transcribe_service
 else:
     print("Services file does not exist")
     
